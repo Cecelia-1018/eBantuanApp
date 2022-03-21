349c950f28d3e7051da0ff88915aa000673bc625
Cecelia-1018 <53387160+Cecelia-1018@users.noreply.github.com>
2022-03-21 14:40:21 +0800

    add top tab

--- ebantuan/src/modules/Donation/DonateMoney.js
+++ ebantuan/src/modules/Donation/DonateMoney.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
 import { Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+
+const Tab = createMaterialTopTabNavigator();
+
+function CreditCard() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>CreditCard!</Text>
+      </View>
+    );
+  }
+
+  function StripePay() {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>StripePay</Text>
+      </View>
+    );
+  }
 
 function DonateMoney(){
   
     return (
-      <View style={{
-          flex: 1,
-          justifyContent: "center",
-          alignItems: "center"
-        }}>
-        <Text>DonateMoney</Text>
-      </View>
+       
+        <Tab.Navigator>
+          <Tab.Screen name="Credit Card" component={CreditCard} />
+          <Tab.Screen name="Stripe Pay" component={StripePay} />
+        </Tab.Navigator>
+     
     );
  
 }
